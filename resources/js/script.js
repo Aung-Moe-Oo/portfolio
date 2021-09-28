da45387d0566dbde52bf603a91e3ee7e14d29f29
Aung-Moe-Oo <70925607+Aung-Moe-Oo@users.noreply.github.com>
2021-09-28 09:26:50 +0630

    Add files via upload

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,74 +5,117 @@ $(window).on("load", function () {
   });
 });
 
-$(document).ready(function () {
-  // scroll reveal
-  window.sr = ScrollReveal();
-  sr.reveal(".scroll-left", {
-    origin: "left",
-    duration: 1000,
-    distance: "200px",
-  });
+// aos js
+AOS.init();
 
-  sr.reveal(".scroll-right", {
-    origin: "right",
-    duration: 2000,
-    distance: "200px",
-  });
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
 
-  sr.reveal(".scroll-top", {
-    origin: "bottom",
-    duration: 2000,
-    distance: "200px",
-  });
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
-  sr.reveal(".scroll-bottom", {
-    origin: "top",
-    duration: 2000,
-    distance: "50px",
-  });
-
-  // smooth scroll
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top,
-            },
-            1000,
-            function () {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) {
-                // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              }
-            }
-          );
-        }
-      }
-    });
 });
+
+$(document).ready(function () {
+  // typed js
+  var type = new Typed(".typing-text", {
+    strings: ["", "React Developer", "Web designer", "Freelancer"],
+    typeSpeed: 100,
+    loop: true,
+  });
+  $("#about").waypoint(
+    function () {
+      $(".progress-bar").each(function () {
+        $(this).animate(
+          {
+            width: $(this).attr("data-progress") + "%",
+          },
+          2000
+        );
+      });
+      $(this).destroy();
+    },
+    {
+      offset: "bottom-in-view",
+    }
+  ); 
+  
+});
+// carousel
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    marginLeft: 10,
+    nav: true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+});
+
+// smooth scroll
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
